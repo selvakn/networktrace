f15add21891d8e2b8e9e5cad166efc160fa40205
Selva <k.n.selvakumar@gmail.com>
2014-09-01 15:36:10 +0530

    Finzalied trace script, logstash config to import to elasticsearch

--- trace.d
+++ trace.d
@@ -2,30 +2,10 @@
 
 #pragma D option quiet
 
-dtrace:::BEGIN
+
+BEGIN
 {
-	err[0]            = "Success";
-
-	err[EAGAIN]       = "EAGAIN";
-	err[EBADF] = "EBADF";
-	err[EFAULT] = "EFAULT";
-	err[EINVAL] = "EINVAL";
-	err[ENOMEM] = "ENOMEM";
-	err[ENOTCONN] = "ENOTCONN";
-	err[ENOTSOCK] = "ENOTSOCK";
-
-
-	err[EINTR]        = "Interrupted syscall";
-	err[EIO]          = "I/O error";
-	err[EACCES]       = "Permission denied";
-	err[ENETDOWN]     = "Network is down";
-	err[ENETUNREACH]  = "Network unreachable";
-	err[ECONNRESET]   = "Connection reset";
-	err[ECONNREFUSED] = "Connection refused";
-	err[ETIMEDOUT]    = "Timed out";
-	err[EHOSTDOWN]    = "Host down";
-	err[EHOSTUNREACH] = "No route to host";
-	err[EINPROGRESS]  = "In progress";
+  last = walltimestamp/1000000;
 }
 
 syscall::connect*:entry
@@ -45,17 +25,15 @@ syscall::recvmsg:return,
 syscall::recvfrom:return,
 syscall::sendmsg:return,
 syscall::sendto:return
-/self->fd && execname != "ocspd"/
+/self->fd && execname != "ocspd" && errno == 0/
 {
-  self->start = walltimestamp / 1000000;
-  this->errstr = err[errno] != NULL ? err[errno] : lltostr(errno);
-  this->size = errno == 0 ? arg0 : 0;
+  @stats[last, probefunc, probename, execname, pid,  self->socks->sa_data[2],  self->socks->sa_data[3],  self->socks->sa_data[4],  self->socks->sa_data[5]] = sum(arg0);
 
-  printf("{\"timestamp\": %ld, \"type\":\"%s:%s\", \"execname\":\"%s\", \"pid\":%d, \"size\":%d, \"error\": \"%s\", \"IP\": \"%d.%d.%d.%d\"}\n",
-  self->start, probefunc, probename, execname, pid, this->size, this->errstr,
-	self->socks->sa_data[2],
-	self->socks->sa_data[3],
-	self->socks->sa_data[4],
-	self->socks->sa_data[5]
-	);
+}
+
+tick-10sec
+{
+  printa("{\"timestamp\": %ld, \"type\":\"%s:%s\", \"execname\":\"%s\", \"pid\":%d, \"IP\": \"%d.%d.%d.%d\", \"size\":%@d}\n", @stats);
+  trunc(@stats);
+  last = walltimestamp/1000000;
 }
